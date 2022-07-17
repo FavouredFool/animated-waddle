ec527810c6d7044c57436f0a8dad303e74fe72fc
Tim Wahrburg <t.wahrburg@gmail.com>
2022-07-17 11:11:48 +0200

    feat: Added Main Menu

---
+++ animated-waddle/Assets/Scripts/BlackBars.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackBars : MonoBehaviour
+{
+    Camera thisCamera;
+    public Vector2 resolution = new Vector3(1920f, 1080f);
+    void Update()
+    {
+        thisCamera = GetComponent<Camera>();
+        Vector2 resTarget = resolution;
+        Vector2 resViewport = new Vector2(Screen.width, Screen.height);
+        Vector2 resNormalized = resTarget / resViewport;
+        Vector2 size = resNormalized / Mathf.Max(resNormalized.x, resNormalized.y);
+        thisCamera.rect = new Rect(default, size) { center = new Vector2(0.5f, 0.5f) };
+    }
+}
